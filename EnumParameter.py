25dcf75bb5fddf51c9aef7af13b45c5ee7121219
HernanRodriguez1 <66162160+HernanRodriguez1@users.noreply.github.com>
2024-03-30 23:25:37 -0500

    Add files via upload

---
+++ EnumParameter.py
@@ -0,0 +1,114 @@
+import argparse
+import requests
+import concurrent.futures
+from urllib.parse import urlparse, parse_qs, urlunparse
+from colorama import Fore, Style
+
+print(Fore.RED + """\n
+    ______                      ____                                  __           
+   / ____/___  __  ______ ___  / __ \____ __________ _____ ___  ___  / /____  _____
+  / __/ / __ \/ / / / __ `__ \/ /_/ / __ `/ ___/ __ `/ __ `__ \/ _ \/ __/ _ \/ ___/
+ / /___/ / / / /_/ / / / / / / ____/ /_/ / /  / /_/ / / / / / /  __/ /_/  __/ /    
+/_____/_/ /_/\__,_/_/ /_/ /_/_/    \__,_/_/   \__,_/_/ /_/ /_/\___/\__/\___/_/     
+                                                                                 
+Create By: Hernan Rodriguez | Team Offsec Peru \n""" + Style.RESET_ALL)
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+
+def conector(url):
+    try:
+        with requests.Session() as session:
+            session.headers.update({'User-Agent': USER_AGENT})
+            respuesta = session.get(url, timeout=5)
+            return respuesta.text, respuesta.status_code, respuesta.headers
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return "", 404, {}  # Retorna código de estado 404 en caso de excepción
+
+def procesar_url(url, lista_negra, args, urls_validas, urls_imprimidas, archivo_salida):
+    parsed = urlparse(url)
+    if parsed.netloc == args.dominio and parsed.query:
+        query_parseado = parse_qs(parsed.query)
+        if all(extension not in url for extension in lista_negra) and any(query_parseado.values()):
+            query_modificada = {k: [''] for k in query_parseado.keys()}
+            url_modificada = urlunparse((parsed.scheme, parsed.netloc, parsed.path, '', '&'.join([f"{k}={v[0]}" for k, v in query_modificada.items()]), ''))
+            if args.verificar:
+                if url_modificada not in urls_imprimidas:
+                    urls_imprimidas.add(url_modificada)
+                    contenido, status_code, headers = conector(url_modificada)
+                    if status_code in {200, 301, 302, 401, 403, 500}:  # Se consideran válidas las respuestas 200, 301, 302, 401, 403 y 500
+                        urls_validas.add(url_modificada)
+                        if status_code == 200:
+                            print(f"[{Fore.GREEN}+{Style.RESET_ALL}] {url_modificada}")
+                        else:
+                            print(f"[{Fore.GREEN}+{Style.RESET_ALL}] {url_modificada} - Código de estado: {status_code}")
+                        with open(archivo_salida, "a") as file:
+                            file.write(f"{url_modificada}\n")
+            else:
+                if url_modificada not in urls_imprimidas:
+                    urls_imprimidas.add(url_modificada)
+                    print(f"{url_modificada}")
+
+def principal():
+    parser = argparse.ArgumentParser(description='Script para obtener URLs de Wayback Machine')
+    parser.add_argument('-t', '--dominio', type=str, help='Especifica el dominio a buscar en Wayback Machine', required=True)
+    parser.add_argument('-e', '--excluir', type=str, help='Especifica las extensiones para excluir, separadas por comas')
+    parser.add_argument('-o', '--salida', type=str, help='Especifica el archivo para exportar las URLs válidas')
+    parser.add_argument('-v', '--verificar', action='store_true', help='Verifica si las URL resultantes son accesibles')
+
+    parser.add_argument('--subsitios', action='store_true', help='Incluir subdominios en la búsqueda')
+    parser.add_argument('--intentos', type=int, default=3, help='Número de reintentos en caso de conexión fallida')
+
+    args = parser.parse_args()
+
+    if args.subsitios or args.subsitios == "True":
+        url = f"https://web.archive.org/cdx/search/cdx?url=*.{args.dominio}/*&output=txt&fl=original&collapse=urlkey&page=/"
+    else:
+        url = f"https://web.archive.org/cdx/search/cdx?url={args.dominio}/*&output=txt&fl=original&collapse=urlkey&page=/"
+
+    reintentar = True
+    intentos = 0
+    respuesta = ""
+    while reintentar and intentos <= args.intentos:
+        respuesta, _, _ = conector(url)  # Solo necesitamos el contenido de la respuesta, por lo que ignoramos el código de estado y las cabeceras
+        if not respuesta:
+            reintentar = True
+            intentos += 1
+        else:
+            reintentar = False
+    if not respuesta:
+        return
+
+    lista_negra = set()  # Inicializar lista_negra como un conjunto
+    urls_validas = set()  # Inicializar urls_validas como un conjunto
+    urls_imprimidas = set()  # Inicializar urls_imprimidas como un conjunto
+
+    if args.excluir:
+        if "," in args.excluir:
+            lista_negra = {extension.strip() for extension in args.excluir.split(",")}
+        else:
+            lista_negra.add(args.excluir)
+
+    if args.excluir:
+        print(f"\u001b[31m[!] Las URL que contienen estas extensiones se excluirán de los resultados: {lista_negra}\u001b[0m\n")
+        args.excluir = None
+    else:
+        print("[!] No hay extensiones para excluir.")
+        args.excluir = None
+
+    # Banner grabbing del servidor
+    contenido, _, headers = conector(f"http://{args.dominio}")
+    print(f"Cabeceras válidas: {headers}\n")
+
+    # Procesamiento adicional para las URLs y exportación si es necesario
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(procesar_url, url, lista_negra, args, urls_validas, urls_imprimidas, args.salida): url for url in set(respuesta.split())}
+        for future in concurrent.futures.as_completed(future_to_url):
+            pass
+
+    args.dominio = None
+    args.subsitios = None
+    args.intentos = None
+
+if __name__ == '__main__':
+    principal()
